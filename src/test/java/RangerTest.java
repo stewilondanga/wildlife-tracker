a8fc9d441cf8afc80a872ab469cdb68668a737a5
Stewart ilondanga <stilondanga@gmail.com>
2018-01-08 16:11:22 +0300

    add build.gradle

--- src/test/java/RangerTest.java
+++ src/test/java/RangerTest.java
@@ -200,5 +200,14 @@ public class RangerTest {
     assertEquals(1, savedRanger.getBadge());
   }
 
+  @Test
+  public void update_preservesOriginalBadge_1() {
+    Ranger testRanger = new Ranger("User", "Mjanja", "Mwenyewe", 1, 5035550000L);
+    testRanger.save();
+    testRanger.update();
+    Ranger savedRanger = Ranger.find(testRanger.getId());
+    assertEquals(1, savedRanger.getBadge());
+  }
+
   
 }
