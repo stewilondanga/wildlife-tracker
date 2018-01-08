53dd20b10b0357f5e4e41b0b09b668f20f1a6404
Stewart ilondanga <stilondanga@gmail.com>
2018-01-08 12:18:51 +0300

    add build.gradle

---
+++ src/test/java/RegularAnimalTest.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.sql.Timestamp;
