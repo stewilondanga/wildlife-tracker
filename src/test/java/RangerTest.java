b70fa6d4b1f2c8d3d967ee328583a8d22b55fd15
Stewart ilondanga <stilondanga@gmail.com>
2018-01-08 14:29:14 +0300

    add build.gradle

---
+++ src/test/java/RangerTest.java
@@ -0,0 +1,7 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+import java.sql.Timestamp;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
