e889fb08e0c78d3a22a1bccd937141d9ff7e4e19
Stewart ilondanga <stilondanga@gmail.com>
2018-01-08 10:19:34 +0300

    add build.gradle

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -9,4 +9,16 @@ publc class SightingTest {
   private RegularAnimal testAnimal;
   private Location testLocation;
   private Ranger testRanger;
+
+  @Rule
+  public void setUp() {
+    testAnimal = new RegularAnimal("Rabbit");
+    testAnimal.save();
+    testLocation = new Location("Near bridge", 1.525, -2.311);
+    testLocation.save();
+    testRanger = new Ranger("User", "Mjanja", "Mwenyewe", 1, 5035550000l);
+    testRanger.save();
+  }
+
+  
 }
