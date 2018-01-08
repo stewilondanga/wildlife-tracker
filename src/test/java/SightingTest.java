4a61eda513aa6d578bd850ca1b6f60c32ddc4bb9
Stewart ilondanga <stilondanga@gmail.com>
2018-01-08 10:15:21 +0300

    add build.gradle

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -4,3 +4,9 @@ import java.util.List;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
+
+publc class SightingTest {
+  private RegularAnimal testAnimal;
+  private Location testLocation;
+  private Ranger testRanger;
+}
