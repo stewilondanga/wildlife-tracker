4e42d950be9533c24fdfe50883a15f77583cc927
Stewart ilondanga <stilondanga@gmail.com>
2018-01-09 17:26:19 +0300

    add src/test/java/LocationTest.java

---
+++ src/test/java/EndangeredAnimalTest.java
@@ -0,0 +1,7 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+import java util.List;
+import java.util.Collections;
+import java.sql.Timestamp;
