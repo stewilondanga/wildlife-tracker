0b459831b24419f435ce1d2a0f5b0438b4f4f631
Stewart ilondanga <stilondanga@gmail.com>
2018-01-09 10:43:35 +0300

    add src/test/java/LocationTest.java

--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -11,5 +11,12 @@ public class LocationTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  // Instantiation
+  @Test
+  public void ranger_instantiatesCorrectly_true() {
+    Location testLocation = new Location("Near bridge", 1.525, -2.311);
+    assertTrue(testLocation instanceof Location);
+  }
+
   
 }
