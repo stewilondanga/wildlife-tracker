dc9f0e9e807c3c53d675b277fe538c35dd53fa8d
Stewart ilondanga <stilondanga@gmail.com>
2018-01-10 09:47:40 +0300

    add src/test/java/LocationTest.java

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -271,5 +271,18 @@ public class EndangeredAnimalTest {
     assertTrue(Animal.idExists(firstAnimal.getId()));
   }
 
-  
+  @Test
+  public void getSightings_getsSightingAssociatedWithId_Sighting() {
+    EndangeredAnimal testAnimal = new EndangeredAnimal("Rabbit", 1.5, "Good");
+    testAnimal.save();
+    Location testLocation = new Location("Near bridge", 1.525, -2.311);
+    testLocation.save();
+    Ranger testRanger = new Ranger("User", "Bob", "Smith", 1, 5035550000L);
+    testRanger.save();
+    Sighting testSighting = new SIghting(testAnimal.getId(), testLocation.getId(), testRanger.getId(), new Timestamp(1L));
+    testSighting.save();
+    List<Sighting> foundSighting = testAnimal.getSightings();
+    Sighting[] expected = { testSighting };
+    assertTrue(foundSighting.containsAll(Arrays.asList(expected)));
+  }
 }
